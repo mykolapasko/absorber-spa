34ce7d60918699cc0477172f56aaedc18b107345
mykolapasko <mykolapasko@gmail.com>
2019-07-08 10:35:30 +0300

    added pipe field in edit formgroup

--- src/public/edit/edit.component.js
+++ src/public/edit/edit.component.js
@@ -29,6 +29,7 @@ function EditItemsComponentController (DataService, $scope) {
     item.data.abs_weight_calc = parseFloat(((hight * density * 3.14 * (item.data.diameter_avg * item.data.diameter_avg)/4)/1000).toPrecision(4));
     item.data.nozzle_avg = Math.round(((parseFloat(item.data.diameter_three) + parseFloat(item.data.diameter_four))/2).toPrecision(4)*100)/100;
     item.data.stamp_avg = Math.round(((parseFloat(item.data.diameter_one) + parseFloat(item.data.diameter_two))/2).toPrecision(4)*100)/100;
+    item.data.pipe = item.pipe;
     var promise = DataService.putInfo(item)
     .then($ctrl.remove(index));
   }
