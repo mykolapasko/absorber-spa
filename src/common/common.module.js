f267e1d963b1f01e09a12bbd31668f7e54f8f70d
mykolapasko <mykolapasko@gmail.com>
2020-05-19 12:05:30 +0300

    loader http interceptor added

--- src/common/common.module.js
+++ src/common/common.module.js
@@ -4,8 +4,11 @@
 angular.module('common', [])
 .constant('ApiPath', 'http://localhost:3000')
 // .constant('ApiPath', 'http://192.168.0.1:3000')
-;
+.config(config);
 
-
+config.$inject = ['$httpProvider'];
+function config($httpProvider) {
+  $httpProvider.interceptors.push('loadingHttpInterceptor');
+}
 
 })();
